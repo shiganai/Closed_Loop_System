14a3fb533419f909e9de7fc2505d3d5645e73d57
shiganai <shiganai.hiro@gmail.com>
2021-05-31 19:24:28 +0900

    Delete spring system

--- triangle_3d/ddt.m
+++ triangle_3d/ddt.m
@@ -15,6 +15,5 @@ dphi3 = q(12);
 
 
 [ddth1,ddth2,ddth3,ddphi1,ddphi2,ddphi3] = find_Dds(dphi1,dphi2,dphi3,dth1,dth2,dth3,g,l_Arm,m1,m2,m3,phi1,phi2,phi3,tau1,tau2,tau3,th1,th2,th3);
-% [ddth1,ddth2,ddth3,ddphi1,ddphi2,ddphi3] = find_Dds_With_Spring(dphi1,dphi2,dphi3,dth1,dth2,dth3,g,k,l_Arm,m1,m2,m3,phi1,phi2,phi3,tau1,tau2,tau3,th1,th2,th3);
 
 dotq = [dth1, ddth1, dth2, ddth2, dth3, ddth3, dphi1, ddphi1, dphi2, ddphi2, dphi3, ddphi3]';
